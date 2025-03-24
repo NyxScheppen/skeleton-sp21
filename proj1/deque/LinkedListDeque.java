d5c477848ba71dbddae2b4a7d77de63e2c1b4a38
NyxScheppen <43686425@qq.com>
2025-03-25 02:01:50 +0800

    aaa

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -141,19 +141,14 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
     }
 
     public boolean equals(Object o) {
-        if (o instanceof LinkedListDeque<?> otherList) {
-            if (this.size != otherList.size) {
+        if (!(o instanceof LinkedListDeque) || ((LinkedListDeque<?>) o).size() != size) {
+            return false;
+        }
+        for (int i = 0; i < size; i += 1) {
+            if (((LinkedListDeque<?>) o).get(i) != get(i)) {
                 return false;
             }
-            int i = 0;
-            for (T item : this) {
-                if (!item.equals(otherList.get(i))) {
-                    return false;
-                }
-                i++;
-            }
-            return true;
         }
-        return false;
+        return true;
     }
 }
